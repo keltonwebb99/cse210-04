6721a8e37c8d26f1b5dd0d80bb961cd6c37d9ea5
TeigenB <barber.teigen@gmail.com>
2022-11-03 11:58:03 -0600

    adding files

---
+++ game/casting/actor.py
@@ -0,0 +1,19 @@
+"""    -actor: (Teigen)
+      A visible, moveable thing that participates in the game. 
+              
+              methods:
+                -get_color:
+                -get_font_size:
+                -get_position:
+                -get_velocity:
+                -move_next:
+                -set_color:
+                -set_position:
+                -set_velocity:
+                  
+    
+       --gem: child of actor (Teigen)
+          Gems and rocks are removed when the player touches them. 
+      
+       --rock: child of actor (Teigen)
+          Gems and rocks are removed when the player touches them."""
